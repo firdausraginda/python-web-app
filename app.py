5a04cfea4370990f24f51d89b8db64ac9e97bbdc
firdausraginda <ragindafirdaus01@gmail.com>
2022-06-05 21:49:12 +0700

    add delete logic & button

--- app.py
+++ app.py
@@ -81,3 +81,13 @@ def edit(id):
             return redirect(url_for('home_page'))
 
     return render_template('edit.html', post=post)
+
+@app.route('/post/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('home_page'))
