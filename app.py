d81a4fd81ee08849353a86a0e9cbe808faefef1f
firdausraginda <ragindafirdaus01@gmail.com>
2022-06-05 20:51:17 +0700

    create new page to show selected post

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
 import sqlite3
+from werkzeug.exceptions import abort
 
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 
 app = Flask(__name__)
@@ -26,3 +36,8 @@ def home_page():
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
