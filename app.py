cd1825792a8310378e6982ac9a3a6cc077893d4f
firdausraginda <ragindafirdaus01@gmail.com>
2022-06-05 21:34:59 +0700

    create edit form & logic

--- app.py
+++ app.py
@@ -60,3 +60,24 @@ def create():
             return redirect(url_for('home_page'))
 
     return render_template('create.html')
+
+@app.route('/post/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('home_page'))
+
+    return render_template('edit.html', post=post)
